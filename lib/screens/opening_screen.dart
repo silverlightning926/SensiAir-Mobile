ec4fc5c6ba0dd66818111b2103893d9dec0ab26f
Sidd Rao <siddrao.926@gmail.com>
2022-09-21 16:55:05 +0530

    Fixed Opening Screen Button

--- lib/screens/opening_screen.dart
+++ lib/screens/opening_screen.dart
@@ -16,71 +16,71 @@ class OpeningScreen extends StatelessWidget {
           ),
           Scaffold(
             backgroundColor: Colors.transparent,
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      Image.asset(
-                        './assets/sensiableLogo.png',
-                        scale: 2.5,
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Column(
+                  children: [
+                    Image.asset(
+                      './assets/sensiableLogo.png',
+                      scale: 2.5,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'SENSIABLE',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 45,
                       ),
-                      const SizedBox(
-                        height: 20,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    MaterialButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login');
+                      },
+                      color: const Color(0xAA000000),
+                      height: 75,
+                      minWidth: 175,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40.0),
                       ),
-                      const Text(
-                        'SENSIABLE',
-                        textAlign: TextAlign.center,
+                      child: const Text(
+                        'LOGIN',
                         style: TextStyle(
                           color: Colors.white,
-                          fontSize: 45,
+                          fontSize: 20,
                         ),
                       ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      MaterialButton(
-                        onPressed: () {},
-                        color: const Color(0xAA000000),
-                        height: 75,
-                        minWidth: 175,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(40.0),
-                        ),
-                        child: const Text(
-                          'LOGIN',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    MaterialButton(
+                      onPressed: () {},
+                      color: const Color(0xAA000000),
+                      height: 75,
+                      minWidth: 175,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40.0),
+                      ),
+                      child: const Text(
+                        'SIGN UP',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
                         ),
                       ),
-                      const SizedBox(
-                        height: 25,
-                      ),
-                      MaterialButton(
-                        onPressed: () {},
-                        color: const Color(0xAA000000),
-                        height: 75,
-                        minWidth: 175,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(40.0),
-                        ),
-                        child: const Text(
-                          'SIGN UP',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
         ],
