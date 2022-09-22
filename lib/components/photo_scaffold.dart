776c8b41c8b7728b13ee0e5bb2116f5042879a56
Sidd Rao <siddrao.926@gmail.com>
2022-09-22 21:42:12 +0530

    Extracted Photo Scaffold Widget

---
+++ lib/components/photo_scaffold.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class PhotoScaffold extends StatelessWidget {
+  final Widget? body;
+
+  final String imagePath;
+
+  const PhotoScaffold({
+    Key? key,
+    this.body,
+    required this.imagePath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset(
+          imagePath,
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
+        ),
+        Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Colors.transparent,
+          body: body,
+        ),
+      ],
+    );
+  }
+}
