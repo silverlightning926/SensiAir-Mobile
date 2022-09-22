522b19f60c0a23aca4872c497f81b1f14ac3de57
Sidd Rao <siddrao.926@gmail.com>
2022-09-22 21:41:53 +0530

    Extracted Icon Text Field Widget

---
+++ lib/components/icon_text_field.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class IconTextField extends StatelessWidget {
+  final Icon icon;
+  final String hintText;
+
+  final Color textFieldColor;
+  final Color hintTextColor;
+  final Color inputTextColor;
+
+  const IconTextField({
+    Key? key,
+    required this.icon,
+    required this.hintText,
+    this.textFieldColor = const Color(0xAA000000),
+    this.hintTextColor = Colors.grey,
+    this.inputTextColor = Colors.white,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 15,
+        horizontal: 35,
+      ),
+      child: TextField(
+        style: TextStyle(
+          color: inputTextColor,
+        ),
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: textFieldColor,
+          icon: icon,
+          iconColor: Colors.white,
+          border: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(
+              Radius.circular(25),
+            ),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(
+              Radius.circular(25),
+            ),
+          ),
+          hintText: hintText,
+          hintStyle: TextStyle(
+            color: hintTextColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
