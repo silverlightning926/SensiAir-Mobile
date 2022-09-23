312eedc2c0cdedcc19cf63527e60f2e5783d10ae
Sidd Rao <siddrao.926@gmail.com>
2022-09-23 21:57:43 +0530

    Moved Dashboard Tab To Separate File

---
+++ lib/components/home_screen_tabs/dashboard_tab.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/material.dart';
+
+import '../parameter_button.dart';
+
+class DashboardTab extends StatefulWidget {
+  final int currentAQI;
+  final int currentTemp;
+  final int currentHumidity;
+  final int currentCO2;
+  final int currrentPM25;
+  final int currentVOC;
+  final int currentPressure;
+
+  const DashboardTab({
+    Key? key,
+    required this.currentAQI,
+    required this.currentTemp,
+    required this.currentHumidity,
+    required this.currentCO2,
+    required this.currrentPM25,
+    required this.currentVOC,
+    required this.currentPressure,
+  }) : super(key: key);
+
+  @override
+  State<DashboardTab> createState() => _DashboardTabState();
+}
+
+class _DashboardTabState extends State<DashboardTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 20.0,
+              horizontal: 10.0,
+            ),
+            child: Container(
+              padding: const EdgeInsets.all(5.0),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(186, 46, 120, 63),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Column(
+                      children: const [
+                        Text(
+                          'Home',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15,
+                          ),
+                        ),
+                        Text(
+                          'Good',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [
+                        Icon(
+                          Icons.update,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          '1 min ago',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 135,
+                        width: 135,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 2.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              widget.currentAQI.toString(),
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 45,
+                              ),
+                            ),
+                            const Text(
+                              'AQI',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 75,
+                        width: 75,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 2.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: const [
+                            Text(
+                              'Ideal',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 10,
+                              ),
+                            ),
+                            Text(
+                              '50',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 30,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 75,
+                        width: 75,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 2.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+            color: const Color.fromARGB(255, 218, 218, 218),
+            child: GridView.count(
+              padding: const EdgeInsets.all(20.0),
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              children: [
+                ParameterButton(
+                  title: 'Temperature',
+                  value: widget.currentTemp.toString(),
+                  unit: 'C',
+                  icon: const Icon(
+                    Icons.thermostat_rounded,
+                    color: Colors.green,
+                    size: 60,
+                  ),
+                  onPressed: () {},
+                ),
+                ParameterButton(
+                  title: 'Humidity',
+                  value: widget.currentHumidity.toString(),
+                  unit: '%',
+                  icon: const Icon(
+                    Icons.water_drop_rounded,
+                    color: Colors.red,
+                    size: 60,
+                  ),
+                  onPressed: () {},
+                ),
+                ParameterButton(
+                  title: 'CO2',
+                  value: widget.currentCO2.toString(),
+                  unit: 'ppm',
+                  icon: const Icon(
+                    Icons.cloud_rounded,
+                    color: Colors.yellow,
+                    size: 60,
+                  ),
+                  onPressed: () {},
+                ),
+                ParameterButton(
+                  title: 'PM2.5',
+                  value: widget.currrentPM25.toString(),
+                  unit: 'ug/m3',
+                  icon: const Icon(
+                    Icons.air_rounded,
+                    color: Colors.yellow,
+                    size: 60,
+                  ),
+                  onPressed: () {},
+                ),
+                ParameterButton(
+                  title: 'VOC',
+                  value: widget.currentVOC.toString(),
+                  unit: 'ppm',
+                  icon: const Icon(
+                    Icons.bug_report_rounded,
+                    color: Colors.red,
+                    size: 60,
+                  ),
+                  onPressed: () {},
+                ),
+                ParameterButton(
+                  title: 'Pressure',
+                  value: widget.currentPressure.toString(),
+                  unit: '',
+                  icon: const Icon(
+                    Icons.arrow_forward_rounded,
+                    color: Colors.green,
+                    size: 60,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
