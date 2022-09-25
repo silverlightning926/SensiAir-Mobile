7813b563e9023e1890f3260942eed801681da177
Sidd Rao <siddrao.926@gmail.com>
2022-09-25 13:32:17 +0530

    Added Loader Overlay To Screens

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 import '../components/icon_text_field.dart';
 import '../components/photo_scaffold.dart';
@@ -9,100 +10,102 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: PhotoScaffold(
-        imagePath: './assets/images/bgPhoto1.jpeg',
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                Image.asset(
-                  './assets/images/sensiableLogo.png',
-                  scale: 2.5,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  'SENSIABLE',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 45,
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                IconTextField(
-                  icon: Icon(
-                    Icons.account_circle_rounded,
-                    color: Colors.white,
-                    size: 50,
-                  ),
-                  hintText: 'Username',
-                ),
-                IconTextField(
-                  icon: Icon(
-                    Icons.password_rounded,
-                    color: Colors.white,
-                    size: 50,
-                  ),
-                  hintText: 'Password',
-                ),
-              ],
-            ),
-            RoundedButton(
-              buttonText: 'LOGIN',
-              onPressed: () {},
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 25.0,
-                right: 25.0,
-              ),
-              child: Column(
+    return LoaderOverlay(
+      child: SafeArea(
+        child: PhotoScaffold(
+          imagePath: './assets/images/bgPhoto1.jpeg',
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
                 children: [
-                  Row(
-                    children: [
-                      const Text(
-                        'Don\'t have an account?',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/signup');
-                        },
-                        child: const Text(
-                          'Sign Up',
+                  Image.asset(
+                    './assets/images/sensiableLogo.png',
+                    scale: 2.5,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'SENSIABLE',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 45,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  IconTextField(
+                    icon: Icon(
+                      Icons.account_circle_rounded,
+                      color: Colors.white,
+                      size: 50,
+                    ),
+                    hintText: 'Username',
+                  ),
+                  IconTextField(
+                    icon: Icon(
+                      Icons.password_rounded,
+                      color: Colors.white,
+                      size: 50,
+                    ),
+                    hintText: 'Password',
+                  ),
+                ],
+              ),
+              RoundedButton(
+                buttonText: 'LOGIN',
+                onPressed: () {},
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 25.0,
+                  right: 25.0,
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        const Text(
+                          'Don\'t have an account?',
                           style: TextStyle(
                             color: Colors.white,
                             fontSize: 18,
-                            fontWeight: FontWeight.w700,
-                            decoration: TextDecoration.underline,
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  const Divider(
-                    color: Colors.white,
-                    thickness: 1.5,
-                  )
-                ],
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/signup');
+                          },
+                          child: const Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Divider(
+                      color: Colors.white,
+                      thickness: 1.5,
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 import '../components/icon_text_field.dart';
 import '../components/photo_scaffold.dart';
@@ -24,140 +25,142 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: PhotoScaffold(
-        imagePath: './assets/images/bgPhoto1.jpeg',
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  './assets/images/sensiableLogo.png',
-                  scale: 6,
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                const Text(
-                  'SENSIABLE',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 35,
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                IconTextField(
-                  controller: userNameController,
-                  icon: const Icon(
-                    Icons.account_circle_rounded,
-                    color: Colors.white,
-                    size: 50,
-                  ),
-                  hintText: 'Username',
-                ),
-                IconTextField(
-                  controller: emailController,
-                  icon: const Icon(
-                    Icons.email_rounded,
-                    color: Colors.white,
-                    size: 50,
-                  ),
-                  hintText: 'Email',
-                ),
-                IconTextField(
-                  controller: phoneController,
-                  icon: const Icon(
-                    Icons.phone_rounded,
-                    color: Colors.white,
-                    size: 50,
-                  ),
-                  hintText: 'Phone Number',
-                ),
-                IconTextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  icon: const Icon(
-                    Icons.password_rounded,
-                    color: Colors.white,
-                    size: 50,
-                  ),
-                  hintText: 'Password',
-                ),
-              ],
-            ),
-            RoundedButton(
-              buttonText: 'SIGN UP',
-              onPressed: () {
-                FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                        email: emailController.text,
-                        password: passwordController.text)
-                    .then((value) {
-                  final user = <String, dynamic>{
-                    "userName": userNameController.text,
-                    "phone": phoneController.text,
-                  };
-
-                  FirebaseFirestore.instance
-                      .collection("userInfo")
-                      .doc('${value.user?.uid}')
-                      .set(user);
-                }).then((value) => {
-                          Navigator.pushNamedAndRemoveUntil(
-                              context, '/home', (route) => false),
-                        });
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 25.0,
-                right: 25.0,
-              ),
-              child: Column(
+      child: LoaderOverlay(
+        child: PhotoScaffold(
+          imagePath: './assets/images/bgPhoto1.jpeg',
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Row(
-                    children: [
-                      const Text(
-                        'Already have an account?',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/login');
-                        },
-                        child: const Text(
-                          'Login',
+                  Image.asset(
+                    './assets/images/sensiableLogo.png',
+                    scale: 6,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  const Text(
+                    'SENSIABLE',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconTextField(
+                    controller: userNameController,
+                    icon: const Icon(
+                      Icons.account_circle_rounded,
+                      color: Colors.white,
+                      size: 50,
+                    ),
+                    hintText: 'Username',
+                  ),
+                  IconTextField(
+                    controller: emailController,
+                    icon: const Icon(
+                      Icons.email_rounded,
+                      color: Colors.white,
+                      size: 50,
+                    ),
+                    hintText: 'Email',
+                  ),
+                  IconTextField(
+                    controller: phoneController,
+                    icon: const Icon(
+                      Icons.phone_rounded,
+                      color: Colors.white,
+                      size: 50,
+                    ),
+                    hintText: 'Phone Number',
+                  ),
+                  IconTextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    icon: const Icon(
+                      Icons.password_rounded,
+                      color: Colors.white,
+                      size: 50,
+                    ),
+                    hintText: 'Password',
+                  ),
+                ],
+              ),
+              RoundedButton(
+                buttonText: 'SIGN UP',
+                onPressed: () {
+                  FirebaseAuth.instance
+                      .createUserWithEmailAndPassword(
+                          email: emailController.text,
+                          password: passwordController.text)
+                      .then((value) {
+                    final user = <String, dynamic>{
+                      "userName": userNameController.text,
+                      "phone": phoneController.text,
+                    };
+
+                    FirebaseFirestore.instance
+                        .collection("userInfo")
+                        .doc('${value.user?.uid}')
+                        .set(user);
+                  }).then((value) => {
+                            Navigator.pushNamedAndRemoveUntil(
+                                context, '/home', (route) => false),
+                          });
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 25.0,
+                  right: 25.0,
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        const Text(
+                          'Already have an account?',
                           style: TextStyle(
                             color: Colors.white,
                             fontSize: 18,
-                            fontWeight: FontWeight.w700,
-                            decoration: TextDecoration.underline,
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  const Divider(
-                    color: Colors.white,
-                    thickness: 1.5,
-                  )
-                ],
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/login');
+                          },
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Divider(
+                      color: Colors.white,
+                      thickness: 1.5,
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
